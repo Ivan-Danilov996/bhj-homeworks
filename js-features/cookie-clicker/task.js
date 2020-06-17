5770593105f43ca79060036d41d4b5cda3ce7fcf
Ivan Danilov <ivandanilov996@yandex.ru>
2020-06-17 22:34:05 +0300

    cookie-clicker edited

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,21 +1,34 @@
-'use strict'
+"use strict";
 
-const clickerCounter = document.getElementById("clicker__counter")
-const clickerSpeed = document.getElementById("clicker__speed")
-const cookie = document.getElementById("cookie")
+const clickerCounter = document.getElementById("clicker__counter");
+const clickerSpeed = document.getElementById("clicker__speed");
+const cookie = document.getElementById("cookie");
 
-let currentTime = Date.now()
+let currentTime;
+let prependTime;
+let speed;
+let counter = 0;
 
-cookie.onclick = function() {
+cookie.onclick = function () {
+  clickerCounter.textContent = (
+    parseInt(clickerCounter.textContent) + 1
+  ).toString();
 
-    clickerCounter.textContent = (parseInt(clickerCounter.textContent) + 1).toString();
+  if (cookie.width === 200) {
+    cookie.width = 250;
+  } else {
+    cookie.width = 200;
+  }
 
-    if(cookie.width === 200){
-        cookie.width = 250
-    } else {
-        cookie.width = 200
-    }
+  counter++;
 
-    clickerSpeed.textContent = ((Date.now() - currentTime) / 1000) / parseInt(clickerCounter.textContent)
-}
+  if (counter == 1) {
+    prependTime = Date.now();
+  } else {
+    currentTime = Date.now();
+    speed = 1 / ((currentTime - prependTime) / 1000);
+    prependTime = currentTime;
+  }
 
+  clickerSpeed.textContent = speed;
+};
